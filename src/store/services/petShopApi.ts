3fc24730d6bb88eb934a010417b0a7ab4ec841de
Yasemin Caliskan <>
2023-08-23 14:29:14 +0200

    refactor: attempt to implement login

--- src/store/services/petShopApi.ts
+++ src/store/services/petShopApi.ts
@@ -1,18 +1,23 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import type { CreatePet, Pet } from "../../types";
+import type { Pet } from "../../types";
 
 export const petShopApi = createApi({
   reducerPath: "petShopApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080/api" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8080/api/pets",
+    prepareHeaders: (headers, { getState }) => {
+
+    },
+  }),
   tagTypes: ["Pet"],
   endpoints: (builder) => ({
     getPets: builder.query<Pet[], void>({
-      query: () => `pets`,
+      query: () => "",
       providesTags: ["Pet"],
     }),
-    addPet: builder.mutation<Pet, CreatePet>({
+    addPet: builder.mutation<Pet, Omit<Pet, "id" | "adopted">>({
       query: (body) => ({
-        url: "/pets",
+        url: "",
         method: "POST",
         body,
       }),
@@ -20,13 +25,20 @@ export const petShopApi = createApi({
     }),
     adoptPet: builder.mutation<string, Pick<Pet, "id">>({
       query: (pet) => ({
-        url: `pets/${pet.id}`,
+        url: `/${pet.id}`,
         method: "PATCH",
       }),
       invalidatesTags: ["Pet"],
     }),
   }),
 });
-
 export const { useGetPetsQuery, useAddPetMutation, useAdoptPetMutation } =
   petShopApi;
+
+/*
+prepareHeaders:(headers, store)=> {
+  return {
+    username: store.getState().user.username,
+    password: store.getState().user.password,
+  }
+}*/
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { petShopApi } from "./services/petShopApi";
+import { authApi } from "./services/authApi";
 
 export const store = configureStore({
   reducer: {
-    // Add the generated reducer as a specific top-level slice
     [petShopApi.reducerPath]: petShopApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(petShopApi.middleware),
+    getDefaultMiddleware().concat(petShopApi.middleware, authApi.middleware),
 });
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
