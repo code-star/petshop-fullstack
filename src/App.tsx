c5faca73e227259e3264e3c367bb016e0a8d264d
Yasemin Caliskan <>
2023-08-24 10:42:19 +0200

    feat: implement authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React from "react";
-import { Typography, Button } from "@mui/material";
+import { Button } from "@mui/material";
 import { Home } from "./Pages/Home/Home";
 import CreatePetForm from "./Pages/CreatePet/CreatePetForm";
 import AlertPopup from "./components/AlertPopup";
 import "./App.css";
-import SignIn from "./Pages/SignIn";
-import {useLoginMutation} from "./store/services/authApi";
+import SignIn from "./Pages/SignIn/SignIn";
+import { useSelector } from "react-redux";
+import {
+  selectCurrentRole,
+  selectCurrentUser,
+} from "./store/services/authSlice";
+import { Role } from "./types";
+import Header from "./components/Header";
 
 function App() {
- const [loginMutation, { isLoading, data }] = useLoginMutation();
- 
+  const role = useSelector(selectCurrentRole);
+  const user = useSelector(selectCurrentUser);
 
   const [isCreatePetFormVisible, setIsCreatePetFormVisible] =
     React.useState<boolean>(false);
   const [petId, setPetId] = React.useState<string>("");
-
-
 
   const toggleCreatePetForm = () => {
     setIsCreatePetFormVisible((prevState) => !prevState);
@@ -34,30 +38,32 @@ function App() {
         </Typography>
       </header>
       <div className="app-container">
-        <SignIn/>
-        {petId.length > 0 && (
-            <AlertPopup
+        {!role && <SignIn />}
+        {role === Role.Admin && (
+          <section>
+            {petId.length > 0 && (
+              <AlertPopup
                 message={`You successfully add ${petId} !`}
                 severity={"success"}
-            />
+              />
+            )}
+            <Button
+              id="create-pet"
+              variant="contained"
+              onClick={toggleCreatePetForm}
+              sx={{ marginBottom: "1rem", marginRight: "5rem" }}
+            >
+              {isCreatePetFormVisible ? "Hide" : "Create Pet"}
+            </Button>
+            {isCreatePetFormVisible && (
+              <CreatePetForm
+                setIsCreatePetFormVisible={setIsCreatePetFormVisible}
+                setPetId={setPetId}
+              />
+            )}
+          </section>
         )}
-        <Button
-          id="create-pet"
-          variant="contained"
-          onClick={toggleCreatePetForm}
-          sx={{ marginBottom: "1rem", marginRight: "5rem" }}
-        >
-          {isCreatePetFormVisible ? "Hide" : "Create Pet"}
-        </Button>
-        <div>
-          {isCreatePetFormVisible && (
-            <CreatePetForm
-              setIsCreatePetFormVisible={setIsCreatePetFormVisible}
-              setPetId={setPetId}
-            />
-          )}
-          {!isCreatePetFormVisible && <Home />}
-        </div>
+        {!isCreatePetFormVisible && role && user && <Home />}
       </div>
     </div>
   );
